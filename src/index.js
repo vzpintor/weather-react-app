01e6dc597f837ac8f8834b965142074f92c09d44
Orlando Vazquez Pintor <vzpintor@gmail.com>
2021-01-11 13:33:56 -0600

    Se corrige issue para desplegar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { CitiesApp } from "./CitiesApp";
+import {CitiesApp} from "./CitiesApp";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<CitiesApp />, document.getElementById("root"));
